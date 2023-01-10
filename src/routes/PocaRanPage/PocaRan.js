8955e118bbb9c77b6681bc1bb41f3cec37f66e87
CodeBlue0 <piri007@cau.ac.kr>
2023-01-10 18:12:59 +0900

    포카 페이지 배경 생성

--- src/routes/PocaRanPage/PocaRan.js
+++ src/routes/PocaRanPage/PocaRan.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.css"
 
 function PocaRan() {
-  return(
-    <div>PocaRan 페이지</div>
+
+  const [page, setPage] = useState(0);
+
+  return (
+    <div className="entire">
+      <PocaRanAppBar page={page}/>
+      <PocaRanBody page={page}/>  
+      <div className="backRec"/>
+    </div>
+  )  
+}
+
+const PocaRanAppBar = ({page}) => {
+  return (
+    <div className="ranAppBar">
+      <div className="closeIcon"/>
+      <div className="appBarText">{page}/8</div>
+      <div className="moreIcon"></div>
+    </div> 
   )
 }
+
+const PocaRanBody = ({page}) => {
+  return (
+    <div className="ranBody">
+      12321312312312
+    </div> 
+  )
+}
+
 
 export default PocaRan
