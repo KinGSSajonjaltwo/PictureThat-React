2ee119ab313fd78cd2759050bb377479a5beb24d
CodeBlue0 <piri007@cau.ac.kr>
2023-01-11 18:00:36 +0900

    모든 것은 잘 흘러갔다.

--- src/routes/PocaRanPage/PocaRan.js
+++ src/routes/PocaRanPage/PocaRan.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import TinderCard from "react-tinder-card";
 import "./style.css"
 
@@ -26,34 +26,74 @@ const PocaRanAppBar = ({page}) => {
 }
 
 const PocaRanBody = ({page}) => {
-  const datas = ['1', '2', '3', '4'];
+  const db = ['10', '9','8','7','6','5','4','3','2','1'];
+  const [currentIndex, setCurrentIndex] = useState(db.length - 1)
   const [lastDirection, setLastDirection] = useState()
 
-  const swiped = (direction, nameToDelete) => {
-    console.log('removing: ' + nameToDelete)
+  const currentIndexRef = useRef(currentIndex)
+
+  const childRefs = useMemo(
+    () => 
+      Array(db.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+      []
+  )
+
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  }
+  
+  const canGoBack = currentIndex < db.length - 1
+  const canSwipe = currentIndex >= 0
+
+  const swiped = (direction, nameToDelete, index) => {
     setLastDirection(direction)
+    updateCurrentIndex(index - 1);
   }
 
-  const outOfFrame = (name) => {
-    console.log(name + ' left the screen!')
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current)
+    //currentIndexRef.current >= idx && childRefs[idx].current.restoreCard()
+  }
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < db.length) {
+      await childRefs[currentIndex].current.swipe(dir)
+    }
+  }
+
+  const goBack = async () => {
+    if (!canGoBack) return
+    const newIndex = currentIndex + 1
+    updateCurrentIndex(newIndex)
+    await childRefs[newIndex].current.restoreCard()
   }
 
   return (
     <div className="ranBody">
       <div className="middle centerAlign">
-        <div className='cardContainer'>
-          {datas.map((data) => (
-            <TinderCard className="swipe cardBody centerAlign" key={data} onSwipe={(dir) => swiped(dir, data)}
-              onCardLeftScreen={() => outOfFrame()}>
-                <h3>{data}</h3>
+        <div className="cardsHouse">
+          <div className="baseCard cardBody">I'mmmmm Base Card!!!</div>
+          {db.map((data, index) => (
+            <TinderCard 
+              ref={childRefs[index]}
+              className="swipe cardBody centerAlign" 
+              key={data} 
+              onSwipe={(dir) => swiped(dir, data, index)}
+              onCardLeftScreen={() => outOfFrame(data, index)}>
+                {data}
             </TinderCard>
           ))}
           {/* <div className="cardBody centerAlign">123</div> */}
         </div>
       </div>
       <div className="bottomMenu">
-        <div className="rotateIcon icon"/>
-        <div className="playIcon icon"/>
+        <div 
+          style={{opacity: !canGoBack && 0.3}} 
+          className="rotateIcon icon" onClick={goBack}/>
+        <div className="playIcon icon" onClick={swipe}/>
       </div>
     </div> 
   )
