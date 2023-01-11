48546c7a5724aead14ac2d207a682934a0de7819
CodeBlue0 <piri007@cau.ac.kr>
2023-01-11 16:51:53 +0900

    Oh! 놀라운 효과군요

--- src/routes/PocaRanPage/PocaRan.js
+++ src/routes/PocaRanPage/PocaRan.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Cards, { Card } from 'react-swipe-card'
+import TinderCard from "react-tinder-card";
 import "./style.css"
 
 function PocaRan() {
@@ -26,21 +26,30 @@ const PocaRanAppBar = ({page}) => {
 }
 
 const PocaRanBody = ({page}) => {
-  const data = ['Alexandre', 'Thomas', 'Lucien']
+  const datas = ['1', '2', '3', '4'];
+  const [lastDirection, setLastDirection] = useState()
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+  }
+
   return (
     <div className="ranBody">
       <div className="middle centerAlign">
-      <Cards onEnd={() => {}} className='master-root cardBody centerAlign'>
-        {data.map(item => 
-          <Card 
-            onSwipeLeft={() => {}} 
-            onSwipeRight={() => {}}>
-            <h2>{item}</h2>
-          </Card>
-        )}
-      </Cards>
-        {/* <div className="cardBody centerAlign">123</div> */}
-      
+        <div className='cardContainer'>
+          {datas.map((data) => (
+            <TinderCard className="swipe cardBody centerAlign" key={data} onSwipe={(dir) => swiped(dir, data)}
+              onCardLeftScreen={() => outOfFrame()}>
+                <h3>{data}</h3>
+            </TinderCard>
+          ))}
+          {/* <div className="cardBody centerAlign">123</div> */}
+        </div>
       </div>
       <div className="bottomMenu">
         <div className="rotateIcon icon"/>
