fade445b24705417a30ef7e2b10437042f551efc
CodeBlue0 <piri007@cau.ac.kr>
2023-01-11 20:27:31 +0900

    20230111 Project end

--- src/routes/PocaRanPage/PocaRan.js
+++ src/routes/PocaRanPage/PocaRan.js
@@ -1,6 +1,8 @@
 import React, { useMemo, useRef, useState } from "react";
 import TinderCard from "react-tinder-card";
 import "./style.css"
+import "../../components/CardGenerator";
+import { getRandomCardsTest } from "../../components/CardGenerator";
 
 function PocaRan() {
 
@@ -26,15 +28,15 @@ const PocaRanAppBar = ({page}) => {
 }
 
 const PocaRanBody = ({page}) => {
-  const db = ['10', '9','8','7','6','5','4','3','2','1'];
-  const [currentIndex, setCurrentIndex] = useState(db.length - 1)
+  const datas = getRandomCardsTest();
+  const [currentIndex, setCurrentIndex] = useState(datas.length - 1)
   const [lastDirection, setLastDirection] = useState()
 
   const currentIndexRef = useRef(currentIndex)
 
   const childRefs = useMemo(
     () => 
-      Array(db.length)
+      Array(datas.length)
         .fill(0)
         .map((i) => React.createRef()),
       []
@@ -45,7 +47,7 @@ const PocaRanBody = ({page}) => {
     currentIndexRef.current = val;
   }
   
-  const canGoBack = currentIndex < db.length - 1
+  const canGoBack = currentIndex < datas.length - 1
   const canSwipe = currentIndex >= 0
 
   const swiped = (direction, nameToDelete, index) => {
@@ -59,7 +61,7 @@ const PocaRanBody = ({page}) => {
   }
 
   const swipe = async (dir) => {
-    if (canSwipe && currentIndex < db.length) {
+    if (canSwipe && currentIndex < datas.length) {
       await childRefs[currentIndex].current.swipe(dir)
     }
   }
@@ -76,17 +78,17 @@ const PocaRanBody = ({page}) => {
       <div className="middle centerAlign">
         <div className="cardsHouse">
           <div className="baseCard cardBody">I'mmmmm Base Card!!!</div>
-          {db.map((data, index) => (
+          {datas.map((data, index) => (
             <TinderCard 
               ref={childRefs[index]}
               className="swipe cardBody centerAlign" 
-              key={data} 
-              onSwipe={(dir) => swiped(dir, data, index)}
-              onCardLeftScreen={() => outOfFrame(data, index)}>
-                {data}
+              key={data[0]} 
+              onSwipe={(dir) => swiped(dir, data[0], index)}
+              onCardLeftScreen={() => outOfFrame(data[0], index)}>
+                {data[0]}
             </TinderCard>
           ))}
-          {/* <div className="cardBody centerAlign">123</div> */}
+          {/* <div className="cardbody centerAlign">123</div> */}
         </div>
       </div>
       <div className="bottomMenu">
