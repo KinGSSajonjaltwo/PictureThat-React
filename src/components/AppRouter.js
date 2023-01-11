633cde90395bb1b27b13206950e42a6786fd3188
CodeBlue0 <piri007@cau.ac.kr>
2023-01-11 15:39:29 +0900

    bug fix

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,7 +2,6 @@ import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "../routes/HomePage/Home";
 import PocaRan from "../routes/PocaRanPage/PocaRan";
-import Test from "../routes/PocaRanPage copy/PocaRan";
 
 const AppRouter = () => {
   return (
@@ -10,7 +9,6 @@ const AppRouter = () => {
           <Routes>
             <Route path = "/" element = {<Home/>}/>
             <Route path = "/PocaRan" element = {<PocaRan/>}/>
-            <Route path = "/Test" element = {<Test/>}/>
           </Routes>
       </Router>
   )
