629413ce46f417c03a11d8761c1eaf20dc7b213c
CodeBlue0 <piri007@cau.ac.kr>
2023-01-24 16:40:39 +0900

    semibold 해줘

--- src/routes/PocaRanPage/LastCard.js
+++ src/routes/PocaRanPage/LastCard.js
@@ -4,7 +4,7 @@ import "./LastCard.css"
 export const LastCard = () =>{
   return (
     <div className="cardBody centerAlign shadowEffect">
-      <div className="backCardImg middle fontText flexColumn">
+      <div className="backCardImg flexGrow fontText flexColumn">
         <div className="fontBig titleSize centerAlign">
           오늘의 포즈
         </div>
