346c1af81fb808a3fca8d3362bf7d21ecd08361f
CodeBlue0 <piri007@cau.ac.kr>
2023-01-24 21:40:43 +0900

    클립보드 킹왕짱 나 울어 와... 강필중 최고 진짜 레전드 오브 레전드 이게 전설이지 또 뭐가 전설이겠어

--- src/routes/PocaRanPage/LastCard.js
+++ src/routes/PocaRanPage/LastCard.js
@@ -13,12 +13,44 @@ export const LastCard = (datas) => {
     return tmpText;
   }
 
-  const copyEvent = () => {
-      navigator.clipboard.writeText(getText())
-      .then(function () {
-        alert('URL 복사가 완료되었습니다.')
-      })
-  }
+  const doCopy = text => {
+    // 흐음 1.
+    if (navigator.clipboard) {
+      // (IE는 사용 못하고, 크롬은 66버전 이상일때 사용 가능합니다.)
+      navigator.clipboard
+        .writeText(text)
+        .then(() => {
+          alert("클립보드에 복사되었습니다.");
+        })
+        .catch(() => {
+          alert("복사를 다시 시도해주세요.");
+        });
+    } else {
+      // 흐름 2.
+      if (!document.queryCommandSupported("copy")) {
+        return alert("복사하기가 지원되지 않는 브라우저입니다.");
+      }
+
+      // 흐름 3.
+      const textarea = document.createElement("textarea");
+      textarea.value = text;
+      textarea.style.top = 0;
+      textarea.style.left = 0;
+      textarea.style.position = "fixed";
+
+      // 흐름 4.
+      document.body.appendChild(textarea);
+      // focus() -> 사파리 브라우저 서포팅
+      textarea.focus();
+      // select() -> 사용자가 입력한 내용을 영역을 설정할 때 필요
+      textarea.select();
+      // 흐름 5.
+      document.execCommand("copy");
+      // 흐름 6.
+      document.body.removeChild(textarea);
+      alert("클립보드에 복사되었습니다.");
+    }
+  };
   
 
   return (
@@ -38,8 +70,8 @@ export const LastCard = (datas) => {
         </div>
         <div className="centerAlign"><hr/></div>
         <div className="bottomSize centerAlign">
-          <button className="downButtom centerAlign shadowEffect font500" text={getText()} onClick={() => 
-            copyEvent()}>
+          <button className="downButtom centerAlign shadowEffect font500" onClick={() => 
+            doCopy(getText())}>
               키워드 화면 
           </button>
         </div>
