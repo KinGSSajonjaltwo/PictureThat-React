341b24d19226b3d9c86de935352432aa525078b2
minsop <minsoplee2015@gmail.com>
2023-01-24 17:31:59 +0900

    폰트 색상 추가

--- src/routes/PocaRanPage/FirstCard.js
+++ src/routes/PocaRanPage/FirstCard.js
@@ -4,7 +4,7 @@ import "./FirstCard.css"
 export const FirstCard = () =>{
   return (
     
-    <div className="cardImageContainer middleAlign fontText">
+    <div className="cardImageContainer middleAlign font500">
         안내사항
     </div>
 
