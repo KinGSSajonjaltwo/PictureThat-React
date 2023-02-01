90e639d2d6618c9a17191ab2161dd3c0e078fc85
minsop <minsoplee2015@gmail.com>
2023-02-01 15:40:30 +0900

    일단은 작동

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,8 +2,8 @@ import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "../routes/HomePage/Home";
 import PocaRan from "../routes/PocaRanPage/PocaRan";
-import { Cau } from "../routes/CAU8Page/Cau";
-import EventRan from "../routes/EventRanPage/EventRan";
+import  Cau  from "../routes/CAU8Page/Cau";
+import  EventRan  from "../routes/EventRanPage/EventRan";
 
 
 const AppRouter = () => {
